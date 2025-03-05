0e0e1e79e4d7d4286f6fda371a0d75f7a0733c9d
jackfromeast <thisiszhengyu@gmail.com>
2025-03-05 17:43:42 -0500

    [Update] Update the site data.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'dark', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://jackfromeast.site',
+  siteRepo: 'https://github.com/jackfromeast/jackfromeast.site',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
