7d0f4a8fd379fd2e93f75aa466e62c25f743c730
jackfromeast <thisiszhengyu@gmail.com>
2025-08-27 00:00:25 -0400

    [Middle] Add the route for notejoy.com.jackfromeast.site

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,12 @@ export function middleware(request: NextRequest) {
     // Serve the zeplin-poc.html file for any path on this subdomain
     return NextResponse.rewrite(new URL('/exp/zeplin-poc.html', request.url))
   }
-  
+
+  if (hostname === 'notejoy.com.jackfromeast.site') {
+    // Serve the notejoy-poc.html file for any path on this subdomain
+    return NextResponse.rewrite(new URL('/exp/notejoy-poc.html', request.url))
+  }
+
   // Continue with normal processing for other domains
   return NextResponse.next()
 }
