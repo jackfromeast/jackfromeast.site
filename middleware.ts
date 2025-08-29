6343490b6aee5f512a6532b4c24de1becff5ae4b
jackfromeast <thisiszhengyu@gmail.com>
2025-08-28 23:38:32 -0400

    [PoC] Support alink html.

--- middleware.ts
+++ middleware.ts
@@ -10,15 +10,15 @@ export function middleware(request: NextRequest) {
     return NextResponse.rewrite(new URL('/exp/zeplin-poc.html', request.url))
   }
 
-  if (hostname === 'notejoy.com.jackfromeast.site') {
-    // Serve the notejoy-poc.html file for any path on this subdomain
-    return NextResponse.rewrite(new URL('/exp/notejoy-poc.html', request.url))
-  }
-
   // If hostname === 'notejoy.com.jackfromeast.site' and path = "alink.html"
   if (hostname === 'notejoy.com.jackfromeast.site' && request.nextUrl.pathname === '/alink.html') {
     // Serve the alink-poc.html file for this specific path
     return NextResponse.rewrite(new URL('/exp/alink-poc.html', request.url))
+  }
+
+  if (hostname === 'notejoy.com.jackfromeast.site') {
+    // Serve the notejoy-poc.html file for any path on this subdomain
+    return NextResponse.rewrite(new URL('/exp/notejoy-poc.html', request.url))
   }
 
   // Continue with normal processing for other domains
